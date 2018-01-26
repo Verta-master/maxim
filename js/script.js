612b0b8d31f905725aa71b9c3b4f06a61c066978
Verta-master <vertyagina@gmail.com>
2018-01-26 19:41:42 +0600

    Circles

--- js/script.js
+++ js/script.js
@@ -10,3 +10,75 @@ navToggle.addEventListener("click", function() {
     navMain.classList.remove("menu--opened");
   }
 });
+
+//Circles
+var circles = $('.value__circle');
+
+circles.appear({ force_process: true });
+
+circles.on('appear', function() {
+  var circle = $(this);
+  if (!circle.data('inited')) {
+    $('#circle1').circleProgress({
+      value: 0.13,
+      size: 144,
+      startAngle: -Math.PI/2,
+      reverse: true,
+      thickness: 3,
+      fill: '#ffffff',
+      emptyFill: 'rgba(255, 255, 255, 0.3)',
+    }).on('circle-animation-progress', function(event, progress, stepValue) {
+      $(this).find('strong').html(Math.round(stepValue * 100) + '<span>%</span>');
+    });
+
+    $('#circle2').circleProgress({
+      value: 0.47,
+      size: 144,
+      startAngle: -Math.PI/2,
+      reverse: true,
+      thickness: 3,
+      fill: '#ffffff',
+      emptyFill: 'rgba(255, 255, 255, 0.3)',
+    }).on('circle-animation-progress', function(event, progress, stepValue) {
+      $(this).find('strong').html(Math.round(stepValue * 100) + '<span>%</span>');
+    });
+
+    $('#circle3').circleProgress({
+      value: 0.71,
+      size: 144,
+      startAngle: -Math.PI/2,
+      reverse: true,
+      thickness: 3,
+      fill: '#ffffff',
+      emptyFill: 'rgba(255, 255, 255, 0.3)',
+    }).on('circle-animation-progress', function(event, progress, stepValue) {
+      $(this).find('strong').html(Math.round(stepValue * 100) + '<span>%</span>');
+    });
+
+    $('#circle4').circleProgress({
+      value: 0.07,
+      size: 144,
+      startAngle: -Math.PI/2,
+      reverse: true,
+      thickness: 3,
+      fill: '#ffffff',
+      emptyFill: 'rgba(255, 255, 255, 0.3)',
+    }).on('circle-animation-progress', function(event, progress, stepValue) {
+      $(this).find('strong').html(Math.round(stepValue * 100) + '<span>%</span>');
+    });
+
+    $('#circle5').circleProgress({
+      value: 0.38,
+      size: 144,
+      startAngle: -Math.PI/2,
+      reverse: true,
+      thickness: 3,
+      fill: '#ffffff',
+      emptyFill: 'rgba(255, 255, 255, 0.3)',
+    }).on('circle-animation-progress', function(event, progress, stepValue) {
+      $(this).find('strong').html(Math.round(stepValue * 100) + '<span>%</span>');
+    });
+    
+    circle.data('inited', true);
+  }
+});
