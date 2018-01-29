5492c349f58649eef624df3408e5d3c93716184e
Verta-master <vertyagina@gmail.com>
2018-01-29 18:38:43 +0600

    Plate list

--- js/script.js
+++ js/script.js
@@ -14,6 +14,21 @@ $('.menu__item').hover(
     $('.menu__sublist', $this).slideUp();
 });
 
+//List and plate-view buttons
+var btnPlate = document.querySelector('.title__plate');
+var btnList = document.querySelector('.title__list');
+var listView = document.querySelector('.timetable__list');
+btnPlate.addEventListener("click", function() {
+  listView.classList.add('timetable__list--plate');
+  btnList.classList.remove('title__btn-active');
+  btnPlate.classList.add('title__btn-active');
+});
+btnList.addEventListener("click", function() {
+  listView.classList.remove('timetable__list--plate');
+  btnList.classList.add('title__btn-active');
+  btnPlate.classList.remove('title__btn-active');
+});
+
 //Circles
 var circles = $('.value__circle');
 
